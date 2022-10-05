f863c2f4ec10319f30a2a04477bc7ee84e1ea889
MariaMoroz <82988399+MariaMoroz@users.noreply.github.com>
2022-10-05 15:20:52 -0400

    add basePageTest (#6)

---
+++ cypress/e2e/basePageTest.cy.js
@@ -0,0 +1,35 @@
+/// <reference types="Cypress" />
+Cypress.on('uncaught:exception', () => false)
+import BasePage from "../pageObjects/BasePage";
+
+const basePage = new BasePage();
+
+describe('Base Page', () => {
+    before( () => {
+        cy.visit('/');
+    });
+
+    it('Verify the header is visible on BasePage', () =>{
+        basePage.getHeaderText().should('be.visible');
+        basePage.getHeaderSecondText().should('be.visible');
+    });
+
+    it('Verify all menu are visible on BasePage', () =>{
+        basePage.getMenuStart().should('be.visible');
+        basePage.getMenuBrowseLanguages().should('be.visible');
+        basePage.getMenuSearchLanguages().should('be.visible');
+        basePage.getMenuTopLists().should('be.visible');
+        basePage.getMenuGuestbook().should('be.visible');
+        basePage.getMenuSubmitNewLanguage().should('be.visible');
+    });
+
+    it('Verify all Footer  menu are visible  on BasePage', () =>{
+        basePage.getFooterMenuStart().should('be.visible');
+        basePage.getFooterMenuBrowseLanguages().should('be.visible');
+        basePage.getFooterMenuSearchLanguages().should('be.visible');
+        basePage.getFooterMenuTopLists().should('be.visible');
+        basePage.getFooterMenuGuestbook().should('be.visible');
+        basePage.getFooterMenuSubmitNewLanguage().should('be.visible');
+    });
+});
+
