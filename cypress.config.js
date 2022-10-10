e63e59dc8d4107ebbb875973687849e91aa1920f
MaryMoroz <morozova.mary@gmail.com>
2022-10-10 14:24:30 -0400

    delete baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://demo.seat-os.com/',
   },
   env: {
     host: 'https://demo.seat-os.com/',
