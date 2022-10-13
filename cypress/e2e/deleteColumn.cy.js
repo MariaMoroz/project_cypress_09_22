48c79c4fd009f73fe521fa14e065f738ea87a128
MaryMoroz <morozova.mary@gmail.com>
2022-10-12 21:14:33 -0400

    add test deleteColumn

---
+++ cypress/e2e/deleteColumn.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+
+
+describe('Bookings management', () => {
+    before(() => {
+        cy.login(Cypress.env('email'), Cypress.env('password'));
+        cy.get('a[href="/orders/"]').click();
+    })
+
+    it('verify user can delete the column STATUS from table', () => {  
+        cy.get('.dataTables_scrollHeadInner > .table tr th').as('headerTable').should('contain', 'Status');
+        cy.get('#table-columns-settings-default').click();
+        cy.get('input[value="status"]').uncheck();
+        cy.get('button.btn-success').contains('OK').click();
+        cy.get('@headerTable').should('not.contain', 'Status');
+    })  
+})
+
